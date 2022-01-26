a9393f011648f49af7545a7ae364e7fc1b531d0d
KemoNine <kemonine@kemonine.info>
2022-01-26 18:02:15 -0500

    Improve pimoroni trackball support

--- users/artsey/artsey.c
+++ users/artsey/artsey.c
@@ -4,3 +4,109 @@
 #include QMK_KEYBOARD_H
 #include "artsey.h"
 #include "combos.h"
+
+void keyboard_post_init_user(void) {
+#ifdef POINTING_DEVICE_ENABLE
+    pimoroni_trackball_set_rgbw(255, 255, 255, PIMORONI_BRIGHTNESS);
+#endif
+}
+
+#ifdef POINTING_DEVICE_ENABLE
+uint8_t white = 0;
+uint8_t red = 255;
+uint8_t green = 0;
+uint8_t blue = 0;
+
+bool set_scrolling = false;
+report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
+    if (set_scrolling) {
+        mouse_report.h = mouse_report.x;
+        mouse_report.v = mouse_report.y;
+        mouse_report.x = mouse_report.y = 0;
+    }
+    return mouse_report;
+}
+
+void ball_increase_hue(void){
+      if(red!=255&&green!=255&&blue!=255){
+        red =255;
+      }
+      if (red==255&&green<255&&blue==0){
+       green += 15;
+      } else if(green==255&&blue==0&&red>0){
+        red-=15;
+      } else if(red==0&&blue<255&&green==255){
+        blue+=15;
+      } else if(blue==255&&green>0&&red==0){
+        green -= 15;
+      } else if(green == 0&&blue==255&&red<255){
+        red +=15;
+      } else if(green ==0&&blue>0&&red==255){
+        blue -=15;
+      }
+      pimoroni_trackball_set_rgbw(red,green,blue,white);
+}
+
+void decrease_color(void){
+  if (green>0){
+    green-=15;
+  }
+  if (red>0){
+    red-=15;
+  }
+  if (blue>0){
+    blue-=15;
+  }
+  pimoroni_trackball_set_rgbw(red,green,blue,white);
+}
+
+void cycle_white(void){
+  if (white<255){
+    white +=15;
+  } else{
+    white=0;
+  }
+  pimoroni_trackball_set_rgbw(red,green,blue,white);
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record){
+  switch (keycode){
+  case  BALL_HUI:
+    if(record->event.pressed){
+      ball_increase_hue();
+    }
+    break;
+
+  case BALL_WHT:
+    if(record-> event.pressed){
+      cycle_white();
+    }
+    break;
+
+  case BALL_DEC:
+   if(record-> event.pressed){
+      decrease_color();
+    }
+    break;
+
+  case BALL_SCR:
+   if(record->event.pressed){
+     set_scrolling = true;
+   } else{
+     set_scrolling = false;
+   }
+   break;
+
+  case BALL_NCL:
+     record->event.pressed?register_code(KC_BTN1):unregister_code(KC_BTN1);
+     break;
+  case BALL_RCL:
+      record->event.pressed?register_code(KC_BTN2):unregister_code(KC_BTN2);
+      break;
+  case BALL_MCL:
+      record->event.pressed?register_code(KC_BTN3):unregister_code(KC_BTN3);
+      break;
+  }
+  return true;
+}
+#endif

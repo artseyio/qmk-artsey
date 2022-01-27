30bab908174d24858ee80d732278796933c8efdb
Glen Goodwin <losttenko@gmail.com>
2022-01-26 21:13:54 -0500

    added layer toggle combos; added mouse layer

--- users/artsey/layout/combos.h
+++ users/artsey/layout/combos.h
@@ -1,76 +1,90 @@
 #pragma once
+#include "layer_names.h"
+
 enum combos {
-  AR_F,
-  AS_W,
-  RT_G,
-  RS_V,
-  TS_J,
-  ART_D,
-  ATS_Q,
-  RTS_X,
-  EY_C,
-  EI_H,
-  EO_B,
-  YI_U,
-  YO_K,
-  IO_N,
-  EYI_L,
-  EIO_P,
-  YIO_M,
-  ARTS_Z,
-  AYI_QUOT,
-  AY_COMM,
-  AI_DOT,
-  AO_SLSH,
-  COMBO_LENGTH
+AR_F,
+AS_W,
+RT_G,
+RS_V,
+TS_J,
+ART_D,
+ATS_Q,
+RTS_X,
+EY_C,
+EI_H,
+EO_B,
+YI_U,
+YO_K,
+IO_N,
+EYI_L,
+EIO_P,
+YIO_M,
+ARTS_Z,
+AYI_QUOT,
+AY_COMM,
+AI_DOT,
+AO_SLSH,
+EIR_TGN,
+TAY_TGM,
+NAV_BASE,
+MSE_BASE,
+COMBO_LENGTH
 };
 
 uint16_t COMBO_LEN = COMBO_LENGTH;
 
-const uint16_t PROGMEM ar_combo[] = {KC_A, KC_R, COMBO_END};
-const uint16_t PROGMEM as_combo[] = {KC_A, KC_S, COMBO_END};
-const uint16_t PROGMEM rt_combo[] = {KC_R, KC_T, COMBO_END};
-const uint16_t PROGMEM rs_combo[] = {KC_R, KC_S, COMBO_END};
-const uint16_t PROGMEM ts_combo[] = {KC_T, KC_S, COMBO_END};
-const uint16_t PROGMEM art_combo[] = {KC_A, KC_R, KC_T, COMBO_END};
-const uint16_t PROGMEM ats_combo[] = {KC_A, KC_T, KC_S, COMBO_END};
-const uint16_t PROGMEM rts_combo[] = {KC_R, KC_T, KC_S, COMBO_END};
-const uint16_t PROGMEM ey_combo[] = {KC_E, KC_Y, COMBO_END};
-const uint16_t PROGMEM ei_combo[] = {KC_E, KC_I, COMBO_END};
-const uint16_t PROGMEM eo_combo[] = {KC_E, KC_O, COMBO_END};
-const uint16_t PROGMEM yi_combo[] = {KC_Y, KC_I, COMBO_END};
-const uint16_t PROGMEM yo_combo[] = {KC_Y, KC_O, COMBO_END};
-const uint16_t PROGMEM io_combo[] = {KC_I, KC_O, COMBO_END};
-const uint16_t PROGMEM eyi_combo[] = {KC_E, KC_Y, KC_I, COMBO_END};
-const uint16_t PROGMEM eio_combo[] = {KC_E, KC_I, KC_O, COMBO_END};
-const uint16_t PROGMEM yio_combo[] = {KC_Y, KC_I, KC_O, COMBO_END};
-const uint16_t PROGMEM arts_combo[] = {KC_A, KC_R, KC_T, KC_S, COMBO_END};
-const uint16_t PROGMEM ayi_combo[] = {KC_A, KC_Y, KC_I, COMBO_END};
-const uint16_t PROGMEM ay_combo[] = {KC_A, KC_Y, COMBO_END};
-const uint16_t PROGMEM ai_combo[] = {KC_A, KC_I, COMBO_END};
-const uint16_t PROGMEM ao_combo[] = {KC_A, KC_O, COMBO_END};
+const uint16_t PROGMEM ar_combo[] = {KC_A, KC_R,  COMBO_END};
+const uint16_t PROGMEM as_combo[] = {KC_A, KC_S,  COMBO_END};
+const uint16_t PROGMEM rt_combo[] = {KC_R, KC_T,  COMBO_END};
+const uint16_t PROGMEM rs_combo[] = {KC_R, KC_S,  COMBO_END};
+const uint16_t PROGMEM ts_combo[] = {KC_T, KC_S,  COMBO_END};
+const uint16_t PROGMEM art_combo[] = {KC_A, KC_R, KC_T,  COMBO_END};
+const uint16_t PROGMEM ats_combo[] = {KC_A, KC_T, KC_S,  COMBO_END};
+const uint16_t PROGMEM rts_combo[] = {KC_R, KC_T, KC_S,  COMBO_END};
+const uint16_t PROGMEM ey_combo[] = {KC_E, KC_Y,  COMBO_END};
+const uint16_t PROGMEM ei_combo[] = {KC_E, KC_I,  COMBO_END};
+const uint16_t PROGMEM eo_combo[] = {KC_E, KC_O,  COMBO_END};
+const uint16_t PROGMEM yi_combo[] = {KC_Y, KC_I,  COMBO_END};
+const uint16_t PROGMEM yo_combo[] = {KC_Y, KC_O,  COMBO_END};
+const uint16_t PROGMEM io_combo[] = {KC_I, KC_O,  COMBO_END};
+const uint16_t PROGMEM eyi_combo[] = {KC_E, KC_Y, KC_I,  COMBO_END};
+const uint16_t PROGMEM eio_combo[] = {KC_E, KC_I, KC_O,  COMBO_END};
+const uint16_t PROGMEM yio_combo[] = {KC_Y, KC_I, KC_O,  COMBO_END};
+const uint16_t PROGMEM arts_combo[] = {KC_A, KC_R, KC_T, KC_S,  COMBO_END};
+const uint16_t PROGMEM ayi_combo[] = {KC_A, KC_Y, KC_I,  COMBO_END};
+const uint16_t PROGMEM ay_combo[] = {KC_A, KC_Y,  COMBO_END};
+const uint16_t PROGMEM ai_combo[] = {KC_A, KC_I,  COMBO_END};
+const uint16_t PROGMEM ao_combo[] = {KC_A, KC_O,  COMBO_END};
+const uint16_t PROGMEM eir_combo[] = {KC_E, KC_I, KC_R,  COMBO_END};
+const uint16_t PROGMEM tay_combo[] = {KC_T, KC_A, KC_Y,  COMBO_END};
+const uint16_t PROGMEM nav_base_combo[] = {KC_LEFT, KC_UP, KC_RIGHT,  COMBO_END};
+const uint16_t PROGMEM mse_base_combo[] = {KC_BTN2, KC_MS_D, KC_BTN2,  COMBO_END};
 
-combo_t key_combos[23] = {
-    [AR_F] = COMBO(ar_combo, KC_F),
-    [AS_W] = COMBO(as_combo, KC_W),
-    [RT_G] = COMBO(rt_combo, KC_G),
-    [RS_V] = COMBO(rs_combo, KC_V),
-    [TS_J] = COMBO(ts_combo, KC_J),
-    [ART_D] = COMBO(art_combo, KC_D),
-    [ATS_Q] = COMBO(ats_combo, KC_Q),
-    [RTS_X] = COMBO(rts_combo, KC_X),
-    [EY_C] = COMBO(ey_combo, KC_C),
-    [EI_H] = COMBO(ei_combo, KC_H),
-    [EO_B] = COMBO(eo_combo, KC_B),
-    [YI_U] = COMBO(yi_combo, KC_U),
-    [YO_K] = COMBO(yo_combo, KC_K),
-    [IO_N] = COMBO(io_combo, KC_N),
-    [EYI_L] = COMBO(eyi_combo, KC_L),
-    [EIO_P] = COMBO(eio_combo, KC_P),
-    [YIO_M] = COMBO(yio_combo, KC_M),
-    [ARTS_Z] = COMBO(arts_combo, KC_Z),
-    [AYI_QUOT] = COMBO(ayi_combo, KC_QUOT),
-    [AY_COMM] = COMBO(ay_combo, KC_COMM),
-    [AI_DOT] = COMBO(ai_combo, KC_DOT),
-    [AO_SLSH] = COMBO(ao_combo, KC_SLSH),
+combo_t key_combos[27] = {
+[AR_F] = COMBO(ar_combo, KC_F),
+[AS_W] = COMBO(as_combo, KC_W),
+[RT_G] = COMBO(rt_combo, KC_G),
+[RS_V] = COMBO(rs_combo, KC_V),
+[TS_J] = COMBO(ts_combo, KC_J),
+[ART_D] = COMBO(art_combo, KC_D),
+[ATS_Q] = COMBO(ats_combo, KC_Q),
+[RTS_X] = COMBO(rts_combo, KC_X),
+[EY_C] = COMBO(ey_combo, KC_C),
+[EI_H] = COMBO(ei_combo, KC_H),
+[EO_B] = COMBO(eo_combo, KC_B),
+[YI_U] = COMBO(yi_combo, KC_U),
+[YO_K] = COMBO(yo_combo, KC_K),
+[IO_N] = COMBO(io_combo, KC_N),
+[EYI_L] = COMBO(eyi_combo, KC_L),
+[EIO_P] = COMBO(eio_combo, KC_P),
+[YIO_M] = COMBO(yio_combo, KC_M),
+[ARTS_Z] = COMBO(arts_combo, KC_Z),
+[AYI_QUOT] = COMBO(ayi_combo, KC_QUOT),
+[AY_COMM] = COMBO(ay_combo, KC_COMM),
+[AI_DOT] = COMBO(ai_combo, KC_DOT),
+[AO_SLSH] = COMBO(ao_combo, KC_SLSH),
+[EIR_TGN] = COMBO(eir_combo, TG(NAV)),
+[TAY_TGM] = COMBO(tay_combo, TG(MSE)),
+[NAV_BASE] = COMBO(nav_base_combo, TG(BASE)),
+[MSE_BASE] = COMBO(mse_base_combo, TG(BASE))
 };
---
+++ users/artsey/layout/layer_names.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#define LAYER_ID_GLOBAL -1
+#define BASE 0
+#define NUM 1
+#define SYM 2
+#define PAR 3
+#define NAV 4
+#define CUS 5
+#define MSE 6
+#define BIG_SYM 7
+#define BIG_FUN 8

c4a830416a3b46b883575bee0f6834228ebfbdae
KemoNine <kemonine@kemonine.info>
2022-02-18 11:33:10 -0500

    add hold tap y/t/- for ansi/artsey swaps

--- users/artsey/layout/tap_dance.c
+++ users/artsey/layout/tap_dance.c
@@ -1,14 +1,116 @@
 // Copyright (c) 2021 Mike "KemoNine" Crosson
 // SPDX-License-Identifier: Apache-2.0
-
 #include QMK_KEYBOARD_H
 #include "tap_dance.h"
 #include "layer_names.h"
 
+// //////////////////
+// Config/setup/stubs
+
+void dance_artsey_left_finished(qk_tap_dance_state_t *state, void *user_data);
+void dance_artsey_left_reset(qk_tap_dance_state_t *state, void *user_data);
+void dance_artsey_right_finished(qk_tap_dance_state_t *state, void *user_data);
+void dance_artsey_right_reset(qk_tap_dance_state_t *state, void *user_data);
+void dance_40p_finished(qk_tap_dance_state_t *state, void *user_data);
+void dance_40p_reset(qk_tap_dance_state_t *state, void *user_data);
+
+static uint8_t td_state;
+
 qk_tap_dance_action_t tap_dance_actions[] = {
     [TD_SLSH_BK_FW] = ACTION_TAP_DANCE_DOUBLE(KC_SLSH, KC_BSLS),
     [TD_AT_EQUAL] = ACTION_TAP_DANCE_DOUBLE(KC_EQUAL, KC_AT),
-    [TD_ARTSEY_LEFT] = ACTION_TAP_DANCE_LAYER_MOVE(KC_T, LAYER_ID_BASE),
-    [TD_ARTSEY_RIGHT] = ACTION_TAP_DANCE_LAYER_MOVE(KC_Y, LAYER_ID_BASE),
-    [TD_40P] = ACTION_TAP_DANCE_LAYER_MOVE(KC_MINUS, LAYER_ID_40P_BASE),
+    // Only call these on finish/reset as thats the only time tap dance results should fire
+    [TD_ARTSEY_LEFT] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_artsey_left_finished, dance_artsey_left_reset),
+    [TD_ARTSEY_RIGHT] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_artsey_right_finished, dance_artsey_right_reset),
+    [TD_40P] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_40p_finished, dance_40p_reset),
 };
+
+// Determine the tapdance state to return
+int dance_current(qk_tap_dance_state_t *state) {
+    if (state->count == 1) {
+        if (state->interrupted || !state->pressed) return TD_SINGLE_TAP;
+        else return TD_SINGLE_HOLD;
+    }
+
+    if (state->count == 2) return TD_DOUBLE_SINGLE_TAP;
+    if (state->count == 3) return TD_TRIPLE_SINGLE_TAP;
+    else return TD_UNKNOWN; // Any number higher than the maximum state value you return above
+}
+
+// ///////////////////
+// Full implementation
+
+void dance_artsey_left_finished(qk_tap_dance_state_t *state, void *user_data) {
+    td_state = dance_current(state);
+    switch (td_state) {
+        case TD_SINGLE_TAP:
+            register_code(KC_T);
+            break;
+        case TD_SINGLE_HOLD:
+            layer_move(LAYER_ID_BASE);
+            break;
+        default:
+            break;
+    }
+}
+
+void dance_artsey_left_reset(qk_tap_dance_state_t *state, void *user_data) {
+    td_state = dance_current(state);
+    switch (td_state) {
+        case TD_SINGLE_TAP:
+            unregister_code(KC_T);
+            break;
+        default:
+            break;
+    }
+}
+
+void dance_artsey_right_finished(qk_tap_dance_state_t *state, void *user_data) {
+    td_state = dance_current(state);
+    switch (td_state) {
+        case TD_SINGLE_TAP:
+            register_code(KC_Y);
+            break;
+        case TD_SINGLE_HOLD:
+            layer_move(LAYER_ID_BASE);
+            break;
+        default:
+            break;
+    }
+}
+
+void dance_artsey_right_reset(qk_tap_dance_state_t *state, void *user_data) {
+    td_state = dance_current(state);
+    switch (td_state) {
+        case TD_SINGLE_TAP:
+            unregister_code(KC_Y);
+            break;
+        default:
+            break;
+    }
+}
+
+void dance_40p_finished(qk_tap_dance_state_t *state, void *user_data) {
+    td_state = dance_current(state);
+    switch (td_state) {
+        case TD_SINGLE_TAP:
+            tap_code(KC_MINUS);
+            break;
+        case TD_SINGLE_HOLD:
+            layer_move(LAYER_ID_40P_BASE);
+            break;
+        default:
+            break;
+    }
+}
+
+void dance_40p_reset(qk_tap_dance_state_t *state, void *user_data) {
+    td_state = dance_current(state);
+    switch (td_state) {
+        case TD_SINGLE_TAP:
+            unregister_code(KC_MINUS);
+            break;
+        default:
+            break;
+    }
+}
--- users/artsey/layout/tap_dance.h
+++ users/artsey/layout/tap_dance.h
@@ -8,3 +8,10 @@
 #define TD_ARTSEY_LEFT 2
 #define TD_ARTSEY_RIGHT 3
 #define TD_40P 4
+
+#define TD_NONE 0
+#define TD_UNKNOWN 1
+#define TD_SINGLE_TAP 2
+#define TD_SINGLE_HOLD 3
+#define TD_DOUBLE_SINGLE_TAP 4
+#define TD_TRIPLE_SINGLE_TAP 5

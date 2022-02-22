4a6e8fdb3ecbf920f7c0f67ba9e3a23421c56351
KemoNine <kemonine@kemonine.info>
2022-02-21 19:11:10 -0500

    start toggle layer implementation

--- users/artsey/artsey.c
+++ users/artsey/artsey.c
@@ -27,6 +27,36 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 }
             }
             break;
+        case LCK_ALT:
+            if (record->event.pressed) {
+                if (get_mods() & MOD_BIT(KC_LALT)) {
+                    unregister_mods(MOD_BIT(KC_LALT));
+                }
+                else {
+                    register_mods(MOD_BIT(KC_LALT));
+                }
+            }
+            break;
+        case LCK_GUI:
+            if (record->event.pressed) {
+                if (get_mods() & MOD_BIT(KC_LGUI)) {
+                    unregister_mods(MOD_BIT(KC_LGUI));
+                }
+                else {
+                    register_mods(MOD_BIT(KC_LGUI));
+                }
+            }
+            break;
+        case LCK_CTRL:
+            if (record->event.pressed) {
+                if (get_mods() & MOD_BIT(KC_LCTL)) {
+                    unregister_mods(MOD_BIT(KC_LCTL));
+                }
+                else {
+                    register_mods(MOD_BIT(KC_LCTL));
+                }
+            }
+            break;
         case OSM_CTRL:
             if (record->event.pressed) {
                 add_oneshot_mods(MOD_BIT(KC_LCTL));
--- users/artsey/layout/keycodes.h
+++ users/artsey/layout/keycodes.h
@@ -8,8 +8,11 @@
 #define OSM_ALT 10002
 #define OSM_SHIFT 10003
 #define LCK_SHIFT 10004
-#define FUN_1 10005
-#define FUN_2 10006
+#define LCK_ALT 10005
+#define LCK_GUI 10006
+#define LCK_CTRL 10007
+#define FUN_1 10008
+#define FUN_2 10009
 #ifdef POINTING_DEVICE_ENABLE
 #define BALL_HUI 20000
 #define BALL_WHT 20001

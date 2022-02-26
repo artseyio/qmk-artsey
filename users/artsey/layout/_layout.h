cfd4ea5fcdf5daaebec1640d643042e78998547c
Rhys Wong <purple_rw@yahoo.com>
2022-02-26 07:44:24 -0800

    add geekboards/macropad_v2

--- users/artsey/layout/_layout.h
+++ users/artsey/layout/_layout.h
@@ -92,7 +92,11 @@
 #ifdef KEYBOARD_ai03_soyuz
 #define ARTSEY_LAYOUT_WRAPPER(...) LAYOUT_ortho_5x4(__VA_ARGS__)
 #else
+#ifdef KEYBOARD_geekboards_macropad_v2
+#define ARTSEY_LAYOUT_WRAPPER(...) LAYOUT_ortho_2x4(__VA_ARGS__)
+#else
 #define ARTSEY_LAYOUT_WRAPPER(...) LAYOUT(__VA_ARGS__)
+#endif // KEYBOARD_geekboards_macropad_v2
 #endif // KEYBOARD_ai03_soyuz
 #endif // KEYBOARD_boardsource_microdox
 #endif // KEYBOARD_minidox_rev1
